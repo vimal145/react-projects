459efb59410b1a03f811af4ba05fcad7b143bd15
Vimal <vimalranan58@gmail.com>
2024-11-25 22:50:15 +0530

    bgchanger project is completed

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [color, setColor] = useState("olive");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full h-screen " style={{ backgroundColor: color }}>
+      <button
+        onClick={() => {
+          setColor("red");
+        }}
+        className="outline-none px-2 py-2 rounded-2xl text-white"
+        style={{ backgroundColor: "black" }}
+      >
+        red
+      </button>
+      <button
+        onClick={() => {
+          setColor("green");
+        }}
+        className="outline-none px-2 py-2 rounded-2xl text-white"
+        style={{ backgroundColor: "green" }}
+      >
+        green
+      </button>
+      <button
+        onClick={() => {
+          setColor("blue");
+        }}
+        className="outline-none px-2 py-2 rounded-2xl text-white"
+        style={{ backgroundColor: "blue" }}
+      >
+        blue
+      </button>
+      <button
+        onClick={() => {
+          setColor("white");
+        }}
+        className="outline-none px-2 py-2 rounded-2xl text-black"
+        style={{ backgroundColor: "white" }}
+      >
+        white
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
