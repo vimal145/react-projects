678eb2a3db1bffa756a3bc4a24e2955d4e1e2296
Vimal <vimalranan58@gmail.com>
2024-12-07 01:14:28 +0530

    added token and made github api work properly

--- 07reactRouter/src/Components/Github/Github.jsx
+++ 07reactRouter/src/Components/Github/Github.jsx
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { data } from 'react-router-dom'
-import { useLoaderData } from 'react-router-dom'
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function Github() {
-  const data = useLoaderData()
-  //  const [data, setData] = useState([])
-  //   useEffect(() => {
-  //    fetch('https://api.github.com/users/hiteshchoudhary')
-  //    .then(response => response.json())
-  //    .then(data => {
-  //       console.log(data);
-  //       setData(data)
-  //    })
-  //   }, [])
+  const data = useLoaderData(); // Use data loaded by the loader
+
   return (
-    <div className='text-center bg-gray-600 text-white text-3xl p-4'>Github Followers: {data.followers} 
-    <img src={data.avatar_url} alt="git_image" width={300}/>
+    <div className='text-center bg-gray-600 text-white text-3xl p-4'>
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt="git_image" width={300} />
     </div>
-    
-  )
+  );
 }
 
-export default Github
+export default Github;
 
 export const githubInfoLoader = async () => {
-  const response = await fetch('https://api.github.com/users/hiteshchoudhary')
-  return response.json();
-}
+  const token = import.meta.env.VITE_GITHUB_TOKEN;  
+
+  if (!token) {
+    throw new Error('GitHub Token not found');  
+  }
+
+  const response = await fetch('https://api.github.com/users/vimal145', {
+    headers: {
+      Authorization: `token ${token}`,  
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');  
+  }
+
+  return response.json();  
+};
